436e9ccd61da1979b1d6e3053ef11c5c53ff22c6
Aya <eya.benhariz98@gmail.com>
2023-10-13 17:04:18 +0200

    changes_made

--- src/model/main.py
+++ src/model/main.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
-def make_model(model_type="random_forest"):
+def make_model(model_type="bayesian"):
     if model_type == "random_forest":
         return RandomForestClassifier()
     elif model_type == "logistic_regression":
